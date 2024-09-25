a594d91cab3a3a4e7870ad913ac998fa2b999f48
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-09-25 22:36:01 +0200

    refactor: Update rss.xml.js to include sorterPosts in the RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,19 @@
 import rss from "@astrojs/rss";
+import getShortedPosts from "@utils/getSorterPosts";
 import { getCollection } from "astro:content";
 export async function GET(context) {
   const posts = await getCollection("posts");
+  const sorterPosts = getShortedPosts(posts);
   return rss({
     title: "Jorge Balibrea Blog - jbalibrea.dev",
     description:
       "Blog de Jorge Balibrea enfocado en desarrollo web, tecnología y programación en jbalibrea.dev.",
     site: context.site,
-    items: posts.map((post) => ({
+    items: sorterPosts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/posts/${post.slug}/`,
+      link: `/blog/posts/${post.slug}/`,
     })),
     customData: `<language>es-es</language>`,
   });
