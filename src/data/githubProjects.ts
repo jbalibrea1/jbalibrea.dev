061b4c9a029bddb3685b3df073cea3602ca08063
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-09-26 22:21:29 +0200

    refactor: Update project descriptions in githubProjects.ts

--- src/data/githubProjects.ts
+++ src/data/githubProjects.ts
@@ -5,20 +5,35 @@ export const githubProjects: Github[] = [
     name: "jbalibrea.dev - Portfolio",
     url: "https://github.com/jbalibrea1/jbalibrea.dev",
     description:
-      "🚀My personal portfolio built with Astro, TypeScript, React, and TailwindCSS",
+      "🚀 My personal portfolio built with Astro, TypeScript, React, and TailwindCSS",
     tags: ["TypeScript", "Astro", "Tailwind", "React", "Portfolio"],
+  },
+  {
+    name: "short-url-back",
+    url: "https://github.com/jbalibrea1/short-url-back",
+    description:
+      "✂️ Backend for a URL shortener service, implemented using different technologies: Node.js with Express and Go.",
+
+    tags: ["Node.js", "Express", "MongoDB", "Go", "REST API"],
+  },
+  {
+    name: "short-url-front",
+    url: "https://github.com/jbalibrea1/short-url-front",
+    description:
+      "✂️ Frontend for a URL shortener built with Astro, React, and TailwindCSS.",
+    tags: ["React", "TypeScript", "Astro", "Tailwind/shadcn"],
   },
   {
     name: "IdealistaWatch",
     url: "https://github.com/borjamorenoMurciaeduca/proyecto-final-daw",
     description:
-      "🏡Web application to monitor price and properties of Idealista, add notes and shared with other users",
+      "🏡 Web application to monitor price and properties of Idealista, add notes and shared with other users",
     tags: ["React", "Laravel", "MUI", "MySQL", "Clean architecture", "Python"],
   },
   {
     name: "dotfiles",
     url: "https://github.com/jbalibrea1/dotfiles",
-    description: "✨My personal dotfiles for Neovim, Tmux, i3 and more",
+    description: "✨ My personal dotfiles for Neovim, Tmux, i3 and more",
     tags: ["lua", "dotfiles", "neovim", "tmux"],
   },
   {
