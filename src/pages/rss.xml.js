a201f01802f88e971bc0a82353b8c1ba4eb32877
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-09-27 01:53:36 +0200

    refactor: Update rss.xml.js to improve blog description and pubDate handling

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,18 @@
 import rss from "@astrojs/rss";
 import getShortedPosts from "@utils/getSorterPosts";
 import { getCollection } from "astro:content";
+
 export async function GET(context) {
   const posts = await getCollection("posts");
   const sorterPosts = getShortedPosts(posts);
   return rss({
     title: "Jorge Balibrea Blog - jbalibrea.dev",
     description:
-      "Blog de Jorge Balibrea enfocado en desarrollo web, tecnología y programación en jbalibrea.dev.",
+      "Blog de Jorge Balibrea. En este blog comparto mi pasión por el desarrollo y la tecnología en jbalibrea.dev",
     site: context.site,
     items: sorterPosts.map((post) => ({
       title: post.data.title,
-      pubDate: post.data.pubDate,
+      pubDate: post.data.pubDateTime,
       description: post.data.description,
       link: `/blog/posts/${post.slug}/`,
     })),
