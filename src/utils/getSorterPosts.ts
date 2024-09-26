4e9eba550a3261c0328502cce99838d71758135a
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-09-26 20:51:05 +0200

    refactor: Update getSorterPosts to consider modDateTime if available

--- src/utils/getSorterPosts.ts
+++ src/utils/getSorterPosts.ts
@@ -3,7 +3,8 @@ import type { CollectionEntry } from "astro:content";
 const getShortedPosts = (posts: CollectionEntry<"posts">[]) => {
   return posts.sort(
     (a, b) =>
-      new Date(b.data.pubDate).getTime() - new Date(a.data.pubDate).getTime(),
+      new Date(b.data.modDateTime || b.data.pubDateTime).getTime() -
+      new Date(a.data.modDateTime || a.data.pubDateTime).getTime(),
   );
 };
 
