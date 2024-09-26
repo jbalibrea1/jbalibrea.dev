ac06f239cbdd5cea9aeda01281eab8729dbe4694
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-09-26 21:22:07 +0200

    refactor: Update component styles and add new font option
    - Update the Button component styles to use new theme colors and hover effects.
    - Update the Prose component styles to add the font option "font-poppins".
    - Update the Header component styles to use a different font size for smaller screens.
    - Update the Greeting component styles to use a different font weight for the name.
    - Update the TimeLine component styles to fix a typo in the description.
    - Update the PostCard component styles to add a new Tag component for displaying tags.
    - Remove the unused PostCard copy component.
    - Update the GitCard component styles to use a different background gradient and font color.

--- src/components/DateTime.tsx
+++ src/components/DateTime.tsx
@@ -1,53 +1,51 @@
-interface DatetimesProps {
-  pubDatetime: string | Date;
-  modDatetime: string | Date | undefined | null;
-}
-
-interface Props extends DatetimesProps {
-  size?: "sm" | "lg";
-  className?: string;
-}
+import type { DateTimeProps } from "@interfaces/DateTime";
 
 export default function Datetime({
-  pubDatetime,
-  modDatetime,
+  pubDateTime,
+  modDateTime,
   size = "sm",
   className = "",
-}: Props) {
+}: DateTimeProps) {
   return (
-    <div
-      className={`flex items-center space-x-2 opacity-80 ${className}`.trim()}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className={`${
-          size === "sm" ? "scale-90" : "scale-100"
-        } inline-block h-6 w-6 min-w-[1.375rem] fill-black dark:fill-white`}
-        aria-hidden="true"
+    <div className="flex flex-col items-end">
+      <div
+        className={`flex items-center space-x-2 opacity-80 ${className}`.trim()}
       >
-        <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
-        <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
-      </svg>
-      {modDatetime && modDatetime > pubDatetime ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className={`${
+            size === "sm" ? "scale-90" : "scale-100"
+          } inline-block h-6 w-6 min-w-[1.375rem] fill-black dark:fill-white`}
+          aria-hidden="true"
+        >
+          <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
+          <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
+        </svg>
+
         <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-          Updated:
+          <FormattedDatetime
+            pubDateTime={pubDateTime}
+            modDateTime={modDateTime}
+          />
+        </span>
+      </div>
+      {modDateTime && modDateTime > pubDateTime ? (
+        <span className={`italic ${size === "sm" ? "text-xs" : "text-base"}`}>
+          Última actualización
         </span>
       ) : (
-        <span className="sr-only">Published:</span>
+        <span className="sr-only">Publicado</span>
       )}
-      <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-        <FormattedDatetime
-          pubDatetime={pubDatetime}
-          modDatetime={modDatetime}
-        />
-      </span>
     </div>
   );
 }
 
-const FormattedDatetime = ({ pubDatetime, modDatetime }: DatetimesProps) => {
+export const FormattedDatetime = ({
+  pubDateTime,
+  modDateTime,
+}: DateTimeProps) => {
   const myDatetime = new Date(
-    modDatetime && modDatetime > pubDatetime ? modDatetime : pubDatetime,
+    modDateTime && modDateTime > pubDateTime ? modDateTime : pubDateTime,
   );
 
   const date = myDatetime.toLocaleDateString(["es-ES"], {
