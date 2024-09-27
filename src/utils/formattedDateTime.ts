00b61745ac3b3f91b01cb5fb2ce9cf26e20cd826
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-09-27 19:06:30 +0200

    refactor: Add formattedDate and formattedTime functions to DateTime component

---
+++ src/utils/formattedDateTime.ts
@@ -0,0 +1,18 @@
+function formattedDate(myDatetime: Date) {
+  const date = myDatetime.toLocaleDateString(["es-ES"], {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+  return date;
+}
+
+function formattedTime(myDatetime: Date) {
+  const time = myDatetime.toLocaleTimeString(["es-ES"], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  return time;
+}
+
+export { formattedDate, formattedTime };
