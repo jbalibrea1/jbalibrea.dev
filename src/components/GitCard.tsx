bc3f37f5b79ae3bf33a7ba04d04fa54cdc77da03
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-10-23 04:22:05 +0200

    Refactor: Update GitCard component to use repoUrl instead of url and improve demo link styling

--- src/components/GitCard.tsx
+++ src/components/GitCard.tsx
@@ -3,7 +3,7 @@ import type { Github } from "@/interfaces/Github";
 import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
 import { useEffect, useState } from "react";
 
-export function GitCard() {
+export function GitCard({ latest = false }) {
   const mouseX = useMotionValue(0);
   const mouseY = useMotionValue(0);
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -23,7 +23,11 @@ export function GitCard() {
     return () => observer.disconnect();
   }, []);
 
-  return githubProjects.map((repo: Github) => (
+  const projects = latest
+    ? githubProjects.filter((repo) => repo.pinned).slice(0, 4)
+    : githubProjects;
+
+  return projects.map((repo: Github) => (
     <article
       key={repo.repoUrl}
       onMouseMove={(e) => {
@@ -38,20 +42,20 @@ export function GitCard() {
         className="pointer-events-none absolute -inset-px rounded-xl opacity-0 transition duration-300 group-hover:opacity-100"
         style={{
           background: useMotionTemplate`
-            radial-gradient(200px circle at ${mouseX}px ${mouseY}px, ${isDarkMode ? "rgba(45,134,119,0.1)" : "rgba(200,200,200,0.25)"}, transparent 80%)
+            radial-gradient(200px circle at ${mouseX}px ${mouseY}px, ${isDarkMode ? "rgba(45,134,119,0.1)" : "rgba(45,134,119,0.10)"}, transparent 80%)
           `,
         }}
       />
       <div className="space-y-6 sm:space-y-4 py-4 px-6">
-        <div className="flex flex-col md:flex-row gap-4 flex-wrap">
+        <div className="flex flex-col md:flex-row md:gap-4 flex-wrap">
           <h3 className="font-bold tracking-tight transition duration-75 hover:text-accent inline-block text-accent">
             <a
               href={repo.repoUrl}
               target="_blank"
               rel="noopener noreferrer"
-              className="group-a inline-flex items-center max-w-fit justify-start transition ease-in-out hover:underline decoration-accent-2  decoration-dotted underline-offset-2 opacity-90 hover:opacity-100"
+              className="group-a inline-flex items-center max-w-fit justify-start transition ease-in-out hover:underline decoration-accent-2  decoration-solid underline-offset-4 opacity-90 hover:opacity-100 hover:scale-[1.02]"
             >
-              {repo.name}
+              {repo.title}
               <svg
                 className="w-3 h-3 ms-2 mb-2 transition ease-in-out -rotate-45 group-a-hover:scale-110"
                 aria-hidden="true"
@@ -74,24 +78,9 @@ export function GitCard() {
               href={repo.publicUrl}
               target="_blank"
               rel="noopener noreferrer"
-              className="group-a inline-flex items-center max-w-fit justify-start transition ease-in-out hover:scale-105 hover:underline decoration-accent-2  decoration-dotted underline-offset-2 opacity-90 hover:opacity-100  font-bold tracking-tight  duration-75 hover:text-accent-2 text-accent-2"
+              className="group-a inline-flex items-center max-w-fit justify-start transition ease-in-out hover:scale-[1.02] hover:underline decoration-accent-2  decoration-solid underline-offset-4 opacity-90 hover:opacity-100  font-bold tracking-tight  duration-75 hover:text-accent-2 text-accent-2"
             >
-              Visita la demo
-              {/* <svg */}
-              {/*   className="w-3 h-3 ms-2 mb-2 transition ease-in-out  -rotate-45 group-a-hover:scale-x-110 " */}
-              {/*   aria-hidden="true" */}
-              {/*   xmlns="http://www.w3.org/2000/svg" */}
-              {/*   fill="currentColor" */}
-              {/*   viewBox="0 0 14 10" */}
-              {/* > */}
-              {/*   <path */}
-              {/*     stroke="currentColor" */}
-              {/*     strokeLinecap="round" */}
-              {/*     strokeLinejoin="round" */}
-              {/*     strokeWidth="2" */}
-              {/*     d="M1 5h12m0 0L9 1m4 4L9 9" */}
-              {/*   ></path> */}
-              {/* </svg> */}
+              Visitar demo 👀
             </a>
           )}
         </div>
@@ -105,7 +94,7 @@ export function GitCard() {
               key={tag}
             >
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] dark:bg-[conic-gradient(from_90deg_at_50%_50%,#c2c2c2_0%,#505050_50%,#bebebe_100%)] bg-[conic-gradient(from_90deg_at_50%_50%,#c2c2c2_0%,#505050_50%,#bebebe_100%)]" />
-              <span className="inline-flex h-full w-full items-center justify-center rounded-full bg-slate-50 dark:bg-zinc-900 px-3 py-1 text-xs font-medium dark:text-gray-50 text-gray-900 backdrop-blur-3xl select-all">
+              <span className="inline-flex h-full w-full items-center justify-center rounded-full bg-bgColor px-3 py-1 text-xs font-medium backdrop-blur-3xl">
                 {tag}
               </span>
             </div>
