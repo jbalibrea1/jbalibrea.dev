dd050a8616a102adad1d8bb0e73fbc91f8ed1d5b
Jorge M. Balibrea <jorge.balibrea1@gmail.com>
2025-06-08 02:14:12 +0200

    feat: add table of contents,  add image zoom and  update some stuff (#11)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,8 @@ import type { ClassValue } from 'clsx';
 import clsx from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+// clsx to resolve class conditionals
+// twMerge to merge and remove duplicate Tailwind CSS classes
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,48 +1,48 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from "tailwindcss/defaultTheme";
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
-  darkMode: "selector",
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: 'selector',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
-        accent: "rgb(var(--theme-accent))",
-        "accent-2": "rgb(var(--theme-accent-2))",
-        bgColor: "rgb(var(--theme-bg))",
-        "bgColor-2": "rgb(var(--theme-bg-2))",
-        link: "rgb(var(--theme-link))",
-        quote: "rgb(var(--theme-quote))",
-        textColor: "rgb(var(--theme-text))",
-        header: "rgb(var(--theme-header))",
-        "header-2": "rgb(var(--theme-header-2))",
+        accent: 'rgb(var(--theme-accent))',
+        'accent-2': 'rgb(var(--theme-accent-2))',
+        bgColor: 'rgb(var(--theme-bg))',
+        'bgColor-2': 'rgb(var(--theme-bg-2))',
+        link: 'rgb(var(--theme-link))',
+        quote: 'rgb(var(--theme-quote))',
+        textColor: 'rgb(var(--theme-text))',
+        header: 'rgb(var(--theme-header))',
+        'header-2': 'rgb(var(--theme-header-2))',
       },
       animation: {
-        flip: "flip 6s infinite steps(2, end)",
-        rotate: "rotate 3s linear infinite both",
-        shimmer: "shimmer 8s infinite",
+        flip: 'flip 6s infinite steps(2, end)',
+        rotate: 'rotate 3s linear infinite both',
+        shimmer: 'shimmer 8s infinite',
       },
       keyframes: {
         flip: {
           to: {
-            transform: "rotate(360deg)",
+            transform: 'rotate(360deg)',
           },
         },
         rotate: {
           to: {
-            transform: "rotate(90deg)",
+            transform: 'rotate(90deg)',
           },
         },
         shimmer: {
-          "0%, 90%, 100%": {
-            "background-position": "calc(-100% - var(--shimmer-width)) 0",
+          '0%, 90%, 100%': {
+            'background-position': 'calc(-100% - var(--shimmer-width)) 0',
           },
-          "30%, 60%": {
-            "background-position": "calc(100% + var(--shimmer-width)) 0",
+          '30%, 60%': {
+            'background-position': 'calc(100% + var(--shimmer-width)) 0',
           },
         },
       },
-      scale: ["group-a-hover"],
+      scale: ['group-a-hover'],
       fontFamily: {
         sans: ['"Montserrat"', '"Poppins"', ...defaultTheme.fontFamily.sans],
         poppins: ["'Poppins'", ...defaultTheme.fontFamily.serif],
@@ -50,9 +50,13 @@ export default {
     },
   },
   plugins: [
-    require("@tailwindcss/typography"),
+    require('@tailwindcss/typography'),
     function ({ addVariant }) {
-      addVariant("group-a-hover", ".group-a:hover &");
+      addVariant('group-a-hover', '.group-a:hover &');
+      addVariant('readed', '&.readed'); // Habilita `readed:text-input`
+      addVariant('highlight', '&.highlight'); // Para `highlight:text-primary`
+      addVariant('highlight-bg', '&.highlight-bg');
+      addVariant('readed', '&.readed');
     },
   ],
 };
